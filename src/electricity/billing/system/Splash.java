bcca2983f79482c062b08d76b75ee79c731ced98
Chetan Patil <chetanpatil6303@gmail.com>
2025-04-17 02:33:10 +0530

    update project billing system created login class and added it to the main splash class

--- src/electricity/billing/system/Splash.java
+++ src/electricity/billing/system/Splash.java
@@ -6,18 +6,29 @@ import java.awt.*;
 public class Splash extends JFrame {
     Splash(){
         ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/Splash/Splash.jpg"));
-        Image image1 = imageIcon.getImage().getScaledInstance(600,400,Image.SCALE_DEFAULT);
+        Image image1 = imageIcon.getImage().getScaledInstance(850,600,Image.SCALE_DEFAULT);
         ImageIcon imageIcon2 = new ImageIcon(image1);
         JLabel imagelable = new JLabel(imageIcon2);
 
         add(imagelable);
-        setSize(600,400);
+        setSize(850,600);
         setLocation(500,200); //from center
 
 
 
 
         setVisible(true);
+
+
+        try{
+            Thread.sleep(3000); //to hold for 3sec
+            setVisible(false);
+
+            new Login();
+        } catch (Exception e) {
+            e.printStackTrace();
+//            System.out.println(e);
+        }
 
         //27.29 First video
     }
