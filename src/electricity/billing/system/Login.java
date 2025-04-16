13a235a9820b7e1234a2752754717c224533ce9b
Chetan Patil <chetanpatil6303@gmail.com>
2025-04-17 02:33:10 +0530

    update project billing system created login class and added it to the main splash class

---
+++ src/electricity/billing/system/Login.java
@@ -0,0 +1,15 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setSize(850,600);
+        setLocation(500,0);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
