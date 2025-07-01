747c8ee430461ee916f03f7da7f3adc67035e62b
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-02 01:04:53 +0530

    updated meterInfo class and added components like JButton,JLabel,Choice etc.

--- src/electricity/billing/system/meterInfo.java
+++ src/electricity/billing/system/meterInfo.java
@@ -13,20 +13,20 @@ public class meterInfo extends JFrame  {
 
         setSize(700,500);
         setLocation(400,200);
-        setLayout(null);
-//        setLayout(new BorderLayout());
+//        setLayout(null);
+        setLayout(new BorderLayout());
 
-        JLabel heading,meterNo,meterLoc,meterType,phaseCode,billType;
+        JLabel heading,meterNo,meterLoc,meterType,phaseCode,billType,day;
 
 
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(new Color(213, 181, 123));
-//        add(panel,"West");
+        add(panel,"Center");
 
-        panel.setBounds(0, 0, 700, 500);  // set bounds of panel
-        add(panel);                      // add only once
+//        panel.setBounds(0, 0, 700, 500);  // set bounds of panel
+//        add(panel);                      // add only once
 
 
 
@@ -67,6 +67,65 @@ public class meterInfo extends JFrame  {
         chMeterType.addItem("Solar Meter");
         chMeterType.addItem("Smart Meter");
         panel.add(chMeterType);
+
+
+        phaseCode = new JLabel("Phase Code: ");
+        phaseCode.setBounds(50,200,100,20);
+        panel.add(phaseCode);
+
+        chPhaseCode = new Choice();
+        chPhaseCode.setBounds(180,200,100,20);
+        chPhaseCode.addItem("011");
+        chPhaseCode.addItem("022");
+        chPhaseCode.addItem("033");
+        chPhaseCode.addItem("044");
+        chPhaseCode.addItem("055");
+        chPhaseCode.addItem("066");
+        chPhaseCode.addItem("077");
+        chPhaseCode.addItem("088");
+        chPhaseCode.addItem("099");
+        panel.add(chPhaseCode);
+
+        billType = new JLabel("Bill Type");
+        billType.setBounds(50,240,100,20);
+        panel.add(billType);
+
+        chBillType = new Choice();
+        chBillType.setBounds(180,240,100,20);
+        chBillType.addItem("Normal");
+        chBillType.addItem("Industrial");
+        panel.add(chBillType);
+
+        day = new JLabel("30 Days billing time");
+        day.setBounds(50,280,200,20);
+        panel.add(day);
+
+
+        JLabel note = new JLabel("Note :-");
+        note.setBounds(50,320,100,20);
+        panel.add(note);
+
+        JLabel note1 = new JLabel("By default Bill is calculated 30 days only");
+        note1.setBounds(180,320,300,20);
+        panel.add(note1);
+
+
+        submit = new JButton("Submit");
+        submit.setBounds(120,370,100,25);
+        submit.setBackground(Color.BLACK);
+        submit.setForeground(Color.WHITE);
+        panel.add(submit);
+
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/details.png"));
+        Image image = imageIcon.getImage().getScaledInstance(230,230,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon1 = new ImageIcon(image);
+        JLabel label = new JLabel(imageIcon1);
+
+        add(label,"East");
+
+
+
+
 
 
 
