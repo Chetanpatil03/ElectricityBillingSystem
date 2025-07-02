cd17bd74c764aa32d84cf81eefaf58e8a61d99e6
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-03 02:10:21 +0530

    created deposite_details.java for filling details

---
+++ src/electricity/billing/system/deposite_details.java
@@ -0,0 +1,20 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class deposite_details extends JFrame {
+
+    deposite_details(){
+        super("Deposit Details");
+
+        setBounds(400,200,700,600);
+
+
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new deposite_details();
+    }
+}
